218e21346e932fa36bf1d4c3c879e5bf86435162
ValNaor <roanq.boersma@gmail.com>
2024-11-25 15:53:47 +0100

    Maandag

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
-// JavaScript Document
-console.log("hi");
+document.addEventListener("DOMContentLoaded", function() {
+    const colorDeal = getComputedStyle(document.documentElement).getPropertyValue('--color-deal').trim();
+
+    const listItems = document.querySelectorAll('article ul li');
+
+    listItems.forEach(function(li) {
+        const computedStyle = window.getComputedStyle(li, '::after');
+        
+        if (computedStyle.content !== 'none') {
+            const paragraphs = li.querySelectorAll('p');
+            if (paragraphs.length >= -10) {
+                paragraphs[1].style.color = colorDeal;
+            }
+        }
+    });
+});
